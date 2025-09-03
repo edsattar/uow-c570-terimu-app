7bd5123551192319bd7fc68c2e045275a1352bbc
Anna Savitskaia <annsavitskayloki@gmail.com>
2025-09-03 13:48:36 +1200

    Matching game is added

---
+++ routes/game.tsx
@@ -0,0 +1,9 @@
+import MatchingGame from "../components/ui/MatchingGame";
+
+export default function game() {
+  return (
+    <div className="min-h-dvh flex items-center justify-center bg-gradient-to-br from-blue-100 via-purple-50 to-pink-100 p-4">
+      <MatchingGame />
+    </div>
+  );
+}
